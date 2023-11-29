5edea86512e047185e8c37e0a4ff261bd4a26441
boyaneck <jinxx93@naver.com>
2023-11-29 14:42:18 +0900

    [민용] 로그인 페이지 -> 홈화면으로 변경

---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>로그인 가보즈아 </div>;
+};
+
+export default Login;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,18 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<Navigate replace to="/" />} />
+        {/* <Route path="/login" element={<Home/>} /> */}
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
